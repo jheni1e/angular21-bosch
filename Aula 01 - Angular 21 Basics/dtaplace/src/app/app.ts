044ccac5d6816f6d966342e50988a541419fbdb4
Jhenie <highhalma@gmail.com>
2026-03-27 16:20:20 -0300

    understanding signals and using ngModel

--- Aula 01 - Angular 21 Basics/dtaplace/src/app/app.ts
+++ Aula 01 - Angular 21 Basics/dtaplace/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('dtaplace');
+  protected _date = signal(this.formatDate(new Date()));
+
+  ngOnInit() {
+    console.log(this.title()) //mostra 'dtaplace'
+  }
+
+  formatDate(date: Date): string {
+    return date.toISOString().split('T')[0];
+  }
 }
