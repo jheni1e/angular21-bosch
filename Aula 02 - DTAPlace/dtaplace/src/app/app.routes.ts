50081bdf633d39f72cbbf8b41efbf7db617b237e
Jhenie <highhalma@gmail.com>
2026-04-16 14:17:50 -0300

    routes.ts updated to work with the guard

--- Aula 02 - DTAPlace/dtaplace/src/app/app.routes.ts
+++ Aula 02 - DTAPlace/dtaplace/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Login } from './features/login/login';
 import { authGuard } from './domain/auth-guard';
 
 export const routes: Routes = [
-    { path: '', component: MainPage, canMatch: [authGuard] },
     { path: 'register', component: Subscribe },
     { path: 'login', component: Login },
+    { path: '', component: MainPage, canMatch: [authGuard] },
 ];
